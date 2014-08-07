82b5984b3081dc1b9896f18b4f6501168e7a2397
Alexander Demchenko <alexdemche@gmail.com>
2014-08-07 10:20:34 +0400

    https://github.com/ademchenko/DependenciesTracker/issues/17
    Prepare to release: create nuget packages
    - added CommonAssemblyInfo

--- DependenciesTracker/DependenciesTracker.Interfaces/Properties/AssemblyInfo.cs
+++ DependenciesTracker/DependenciesTracker.Interfaces/Properties/AssemblyInfo.cs
@@ -1,25 +1,8 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
+﻿// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
+using System.Reflection;
+
 [assembly: AssemblyTitle("DependenciesTracking.Interfaces")]
-[assembly: AssemblyDescription("Brings infrastructure for observing property paths changes and automatically updating properties that depend on that paths.")]
-[assembly: AssemblyCompany("Alexander Demchenko")]
-
 [assembly: AssemblyProduct("DependenciesTracking.Interfaces")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ DependenciesTracker/DependenciesTracker.Interfaces/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyDescription("Brings infrastructure for observing property paths changes and automatically updating properties that depend on that paths.")]
+[assembly: AssemblyCompany("Alexander Demchenko")]
+
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
