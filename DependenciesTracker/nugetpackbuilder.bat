40430808e2e34423296abaeb6238a0ca96a8f91a
Alexander Demchenko <alexdemche@gmail.com>
2014-08-15 14:32:52 +0400

    https://github.com/ademchenko/DependenciesTracker/issues/17
    Prepare to release: create nuget packages
    - added portable profile

--- DependenciesTracker/nugetpackbuilder.bat
+++ DependenciesTracker/nugetpackbuilder.bat
@@ -1,5 +1,6 @@
 rmdir /s /q "NuGetPackaging"
-xcopy /Y DependenciesTracker.Interfaces\bin\Debug\DependenciesTracking.Interfaces.dll NuGetPackaging\DependenciesTracker.Interfaces\lib\net45\*
+xcopy /Y DependenciesTracker.Interfaces\bin\Debug\DependenciesTracking.Interfaces.dll NuGetPackaging\DependenciesTracker.Interfaces\lib\portable-net45+netcore45+wp8+MonoAndroid1+MonoTouch1\*
+xcopy /Y DependenciesTracking.Interfaces.Net40\bin\Debug\DependenciesTracking.Interfaces.dll NuGetPackaging\DependenciesTracker.Interfaces\lib\net40\*
 xcopy /Y DependenciesTracker.Interfaces\DependenciesTracking.Interfaces.nuspec NuGetPackaging\DependenciesTracker.Interfaces\*
 cd NuGetPackaging
 nuget pack DependenciesTracker.Interfaces\DependenciesTracking.Interfaces.nuspec
@@ -7,7 +8,8 @@ for %%a in (DependenciesTracking.Interfaces.*.nupkg) do nuget push "%%a"
 ren "*.nupkg" "*.nupkg-pushed"
 
 cd ..
-xcopy /Y DependencyTracker\bin\Debug\DependenciesTracking.dll NuGetPackaging\DependenciesTracker\lib\net45\*
+xcopy /Y DependencyTracker\bin\Debug\DependenciesTracking.dll NuGetPackaging\DependenciesTracker\lib\portable-net45+netcore45+wp8+MonoAndroid1+MonoTouch1\*
+xcopy /Y DependenciesTracking.Net40\bin\Debug\DependenciesTracking.dll NuGetPackaging\DependenciesTracker\lib\net40\*
 xcopy /Y DependencyTracker\DependenciesTracking.nuspec NuGetPackaging\DependenciesTracker\*
 cd NuGetPackaging
 nuget pack DependenciesTracker\DependenciesTracking.nuspec
