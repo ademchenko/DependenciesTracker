f0254f7dae96b10b8cc55b35746c95a75defdba4
Alexander Demchenko <alexdemche@gmail.com>
2014-09-04 09:00:35 +0400

    Prepare to release: the bare minimum of documentation #20
    - added sample project

---
+++ DependenciesTracker/QuickStartSample/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace QuickStartSample
+{
+    public class ViewModel
+    {
+        private readonly Order _order = new Order();
+
+        private readonly Invoice _invoice = new Invoice();
+
+        public Order Order
+        {
+            get { return _order; }
+        }
+
+        public Invoice Invoice
+        {
+            get { return _invoice; }
+        }
+    }
+}
