13aac1b6448e7c65596b54d3fa776855c3398450
Alexander Demchenko <alexdemche@gmail.com>
2015-06-08 22:09:18 +0300

    Prepare to release: the bare minimum of documentation #20
    - fixes in sample project

--- DependenciesTracker/QuickStartSample/Invoice.cs
+++ DependenciesTracker/QuickStartSample/Invoice.cs
@@ -11,9 +11,13 @@ namespace QuickStartSample
     public class Invoice : INotifyPropertyChanged
     {
         private readonly ObservableCollection<Order> _orders = new ObservableCollection<Order>();
-        private decimal _totalQuantity;
+        
         private decimal _totalCost;
-        private Order _candidateOrder = new Order();
+
+        public ObservableCollection<Order> Orders
+        {
+            get { return _orders; }
+        }
 
         public decimal TotalCost
         {
@@ -26,65 +30,14 @@ namespace QuickStartSample
             }
         }
 
-        public decimal TotalQuantity
-        {
-            get { return _totalQuantity; }
-            set
-            {
-                if (value == _totalQuantity) return;
-                _totalQuantity = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public Order CandidateOrder
-        {
-            get { return _candidateOrder; }
-            set
-            {
-                if (Equals(value, _candidateOrder)) return;
-                _candidateOrder = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public ObservableCollection<Order> Orders
-        {
-            get { return _orders; }
-        }
-
         private DelegateCommand _addNewOrderCommand;
-
-        public DelegateCommand AddNewOrderCommand
-        {
-            get { return _addNewOrderCommand ?? (_addNewOrderCommand = new DelegateCommand(_ => AddNewOrderImpl())); }
-        }
-
-        private void AddNewOrderImpl()
-        {
-            Orders.Add(CandidateOrder);
-            CandidateOrder = new Order();
-        }
-
-        private DelegateCommand _deleteOrderCommand;
-
-        public DelegateCommand DeleteOrderCommand
-        {
-            get { return _deleteOrderCommand ?? (_deleteOrderCommand = new DelegateCommand(DeleteOrderImpl)); }
-        }
-
-        private void DeleteOrderImpl(object parameter)
-        {
-            var orderToDelete = (Order) parameter;
-            Orders.Remove(orderToDelete);
-        }
-        
+                
         private static readonly IDependenciesMap<Invoice> _dependenciesMap = new DependenciesMap<Invoice>();
 
         static Invoice()
         {
-            _dependenciesMap.AddDependency(i => i.TotalQuantity, i => i.Orders.Sum(o => o.Quantity), i => i.Orders.EachElement().Quantity)
-                .AddDependency(i => i.TotalCost, i => i.Orders.Sum(o => o.Price * o.Quantity), i => i.Orders.EachElement().Price, i => i.Orders.EachElement().Quantity);
+            _dependenciesMap.AddDependency(i => i.TotalCost, i => i.Orders.Sum(o => o.Price * o.Quantity), 
+                                           i => i.Orders.EachElement().Price, i => i.Orders.EachElement().Quantity);
         }
 
         public Invoice()
@@ -93,8 +46,7 @@ namespace QuickStartSample
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
+        
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChangedEventHandler handler = PropertyChanged;
