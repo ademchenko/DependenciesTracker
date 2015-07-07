aeffa03f1f6d63ceb63e4ea5c5aa8a5bd03bfcc8
Alexander Demchenko <alexdemche@gmail.com>
2015-07-07 06:19:11 +0300

    https://github.com/ademchenko/DependenciesTracker/issues/23 Prepare the release: separate examples project from the main solution
    - finished

--- DependenciesTracker/DependencyTracker/Properties/AssemblyInfo.cs
+++ DependenciesTracker/DependencyTracker/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 [assembly: InternalsVisibleTo("DependenciesTracking.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000fdf2832469587d6699039fd6bfa8dbae85c9ae94fded0d77866c79c41260efbcc48e6453e98eec3bbed499ad4db44c7f84bc8e6c3f0e76cac74a192ac286c24eb697856fd744b5940f48f9e345e513bb77efb8dd8e76a5a063a67d4f999c1afe694d9efc77fd0d5bc5dcd9d2400e7b7e1650253fd2f1d4fef3cc025f2b3e8a3")]
